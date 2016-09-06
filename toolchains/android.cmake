1502097971373a52d4320e4bc616071c38e32b9f
Matt Blair <blair1618@gmail.com>
2016-09-06 18:59:00 -0400

    Remove NDK toolchain checks and debugging config from android.cmake

--- toolchains/android.cmake
+++ toolchains/android.cmake
@@ -1,26 +1,5 @@
 add_definitions(-DPLATFORM_ANDROID)
 
-if(NOT MAKE_BUILD_TOOL)
-    message(SEND_ERROR "Provide ndk make, located in <NDK_ROOT>/prebuilt/<OS>/bin/, -DMAKE_BUILD_TOOL=<NDK_PREBUILT_BIN>/make")
-    return()
-else()
-    set(CMAKE_BUILD_TOOL ${MAKE_BUILD_TOOL})
-    message(STATUS "Will use make prebuilt tool located at : ${CMAKE_BUILD_TOOL}")
-endif()
-
-FILE(READ "$ENV{ANDROID_NDK}/RELEASE.TXT" NDK_VERSION_STRING)
-string(FIND ${NDK_VERSION_STRING} "r10e" NDK_MATCH)
-if(${NDK_MATCH} LESS 0)
-  message(SEND_ERROR "Please use NDK version r10e as $ANDROID_NDK")
-  return()
-endif()
-
-# check for unsupported compilers
-check_unsupported_compiler_version()
-
-# configurations
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -pedantic")
-
 # build external dependencies
 add_subdirectory(${PROJECT_SOURCE_DIR}/external)
 
@@ -29,10 +8,10 @@ add_subdirectory(${PROJECT_SOURCE_DIR}/core)
 
 set(ANDROID_PROJECT_DIR ${PROJECT_SOURCE_DIR}/android/tangram)
 
-if(CMAKE_BUILD_TYPE MATCHES Debug)
-include(toolchains/android.gdb.cmake)
-android_ndk_gdb_enable()
-endif()
+# if(CMAKE_BUILD_TYPE MATCHES Debug)
+# include(toolchains/android.gdb.cmake)
+# android_ndk_gdb_enable()
+# endif()
 
 set(LIB_NAME tangram) # in order to have libtangram.so
 
@@ -59,9 +38,9 @@ target_compile_options(${LIB_NAME}
   PUBLIC
   -fPIC)
 
-if(CMAKE_BUILD_TYPE MATCHES Debug)
-android_ndk_gdb_debuggable(${LIB_NAME})
-endif()
+# if(CMAKE_BUILD_TYPE MATCHES Debug)
+# android_ndk_gdb_debuggable(${LIB_NAME})
+# endif()
 
 # install to android library dir
 set(LIB_INSTALLATION_PATH ${CMAKE_SOURCE_DIR}/android/tangram/libs/${ANDROID_ABI})
